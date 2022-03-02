750e37f10a75ffc3949aad27c3196f001d604f15
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-02 18:13:52 +0100

    add: testing of autocomplete function in target id"d div

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,15 @@
-it('works', () => {
-    
+it('Shows an autocomplete!', () => {
+    createAutoComplete({
+        root: document.querySelector('#target'),
+        fetchData(){
+            return [
+                {Title: 'Avengers'},
+                {Title: 'Not Avengers'},
+                {Title: 'Definitely not Avengers'},
+            ];
+        },
+        renderOption(movie) {
+            return movie.Title;
+        }
+    })
 });
