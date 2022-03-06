1c342cfc070f127ffb7de6479b7d3359877935b6
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-06 19:12:04 +0100

    add: expect asssertion to check dropdown menu is-active

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -13,5 +13,6 @@ it('Shows an autocomplete!', () => {
         }
     });
     const dropdown = document.querySelector('.dropdown');
-
+    
+    expect(dropdown.className).not.to.include('is-active');
 });
